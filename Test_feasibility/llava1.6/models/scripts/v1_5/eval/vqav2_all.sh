b6eed9d0d23b77bba9caac1140a85379eafab6e4
jake <jake630@g.skku.edu>
2024-10-31 22:37:17 +0900

    add rest

--- Test_feasibility/llava1.6/models/scripts/v1_5/eval/vqav2_all.sh
+++
@@ -1,7 +0,0 @@
-# vqav2_all.sh
-
-# Run each script one after another
-bash ./scripts/v1_5/eval/vqav2_zero.sh
-bash ./scripts/v1_5/eval/vqav2_truncated.sh
-bash ./scripts/v1_5/eval/vqav2_interpolation.sh
-bash ./scripts/v1_5/eval/vqav2.sh
