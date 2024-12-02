50f22b78d44dd50b6d0df7b91384071029749fd5
Woohye0n <wh761@g.skku.edu>
2024-12-02 02:47:28 +0000

    add medical

---
+++ lmms-medical/lmms_eval/tasks/slake/utils.py
@@ -0,0 +1,43 @@
+import datetime
+import json
+import os
+from collections import defaultdict
+
+from loguru import logger as eval_logger
+
+def slake_doc_to_visual(doc):
+    return [doc["image"].convert("RGB")]
+
+def slake_doc_to_text(doc):
+    # Assuming the 'doc' dictionary has a key 'question' with the question text
+    question = doc["question"].strip()
+    #return f"{question}\nAnswer the question using a single word or phrase."
+    return f"{question}"
+
+def slake_process_results(doc, results):
+    pred = results[0].lower().strip()
+    gt_ans = doc["answer"].lower().strip()   
+    category = "slake_closed_accuracy" if gt_ans in ["yes", "no"] else "slake_open_accuracy"
+    if category == "slake_closed_accuracy":        
+        score = 1.0 if gt_ans in pred else 0.0
+    else:
+        score = 1.0 if gt_ans in pred else 0.0
+    return {
+        category: {"category": category, "score": score, "prediction": pred, "ground_truth": gt_ans},        
+    }
+
+def slake_aggregate_results(results):
+    category_scores = defaultdict(list)
+    for result in results:
+        score = result["score"]
+        category = result["category"]
+        category_scores[category].append(score)
+
+    category_avg_score = {}
+    for category, scores in category_scores.items():
+        avg_score = sum(scores) / len(scores)
+        category_avg_score[category] = avg_score
+        eval_logger.info(f"{category}: {avg_score:.2f}")
+
+    avg_score = sum(category_avg_score.values()) / len(category_avg_score)
+    return avg_score
---
+++ lmms-medical/run_default.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+
+## generation_type: [recursion, recursion_retain_base, default, base-only, split-only]
+# string "recursion" must be in generation_type for recursive generation
+
+## fix_grid: [default, 2x2] - 2x2: force the image to be square
+## attention_thresholding_type: [layer_mean, layer_mean_with_top_k]
+## remove_unpadding: set True to remove unpadding and set mm_patch_merge_type='spatial' else 'spatial_unpad'
+## regenerate_condition: currently "all": always regenerate when type is "recursion"
+
+# python3 -m accelerate.commands.launch \
+#     --num_processes=3 \
+#     -m lmms_eval \
+#     --model llava \
+#     --model_args pretrained="liuhaotian/llava-v1.6-vicuna-7b" \
+#     --tasks mmbench_en_dev_lite,mme,mmmu_val,mmstar,vqav2_val_lite,realworldqa,pope_pop,textvqa_val_lite,chartqa_lite,seedbench_lite \
+#     --batch_size 1 \
+#     --log_samples \
+#     --log_samples_suffix llava_v1.6 \
+#     --output_path ./logs/ \
+#     --generation_type total \
+#     --fix_grid 2x2 \
+#     --remove_unpadding True \
+#     --attention_thresholding_type layer_mean_topk \
+#     --attention_threshold "0.3" \
+#     --attn_norm None \
+#     --stages "0" "1" \
+#     --verbosity DEBUG \
+#     --wandb_args "project=llava1.6_recursive_eval,entity=VLM_Hallucination_Woohyeon,name=336-672-total" \
+#     # --save_output True \
+#     # --output_csv_path "./generation_output_pope_84-168-336-672-topk-80.csv" \
+#     # --visualize_heatmap True \
+
+# python3 -m accelerate.commands.launch \
+#     --num_processes=1 \
+#     -m lmms_eval \
+#     --device cuda:0 \
+#     --model llava \
+#     --model_args pretrained="liuhaotian/llava-v1.6-vicuna-7b" \
+#     --tasks vqav2_val_lite \
+#     --batch_size 1 \
+#     --log_samples \
+#     --log_samples_suffix llava_v1.6_pope \
+#     --output_path ./logs/ \
+#     --generation_type total \
+#     --fix_grid 2x2 \
+#     --attention_thresholding_type layer_mean_topk \
+#     --attention_threshold 0.3 \
+#     --remove_unpadding True \
+#     --attn_norm None \
+#     --stages "-2" "-1" "0" "1" \
+#     --verbosity DEBUG \
+#     --wandb_args "project=llava1.6_recursive_eval_1126,entity=VLM_Hallucination_Woohyeon,name=82-168-336-672-pad-total" \
+#     # --save_output True \
+#     # --output_csv_path "./generation_output_pope_84-168-336-672-topk-80.csv" \
+#     # --visualize_heatmap True \
+
+python3 -m accelerate.commands.launch \
+    --num_processes=3 \
+    -m lmms_eval \
+    --model llava \
+    --model_args pretrained="liuhaotian/llava-v1.6-vicuna-7b" \
+    --tasks mmbench_en_dev_lite,mme,mmmu_val,mmstar,vqav2_val_lite,realworldqa,pope_pop,textvqa_val_lite,chartqa_lite,seedbench_lite \
+    --batch_size 1 \
+    --log_samples \
+    --log_samples_suffix llava_v1.6 \
+    --output_path ./logs/ \
+    --generation_type total \
+    --fix_grid 2x2 \
+    --remove_unpadding True \
+    --attention_thresholding_type layer_mean_topk \
+    --attention_threshold "0.3" \
+    --attn_norm None \
+    --stages "0" "1" \
+    --verbosity DEBUG \
+    --wandb_args "project=llava1.6_recursive_eval,entity=VLM_Hallucination_Woohyeon,name=336-672-total" \
+    # --save_output True \
+    # --output_csv_path "./generation_output_pope_84-168-336-672-topk-80.csv" \
+    # --visualize_heatmap True \
